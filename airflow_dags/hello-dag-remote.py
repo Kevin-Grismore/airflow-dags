60b63d99bf2fa158d3ae703e87147453cb1ef6a3
Kevin Grismore <kevin.grismore@illumis.com>
2023-05-10 13:46:26 -0400

    rearrange files

---
+++ airflow_dags/hello-dag-remote.py
@@ -0,0 +1,17 @@
+import pendulum
+from airflow.decorators import dag, task
+
+@dag(
+    dag_id="hello-dag-remote",
+    schedule_interval="0 0 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+)
+def hello():
+    @task
+    def hello_task():
+        print('Hello Elena!')
+
+    hello_task()
+
+dag = hello()
